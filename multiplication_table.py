e52066a240fc7d6c7819225e4dda4c128c03eab3
SarangiAditi <70441310+SarangiAditi@users.noreply.github.com>
2020-08-29 21:24:42 +0530

    Update multiplication_table.py

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,3 +1,8 @@
 start=int(input("Table from: "))
 end=int(input("Table to: "))
 print("")
+for i in range(start,end+1):
+  print(f'Table of {i}')
+  print("___________")
+  for j in range(1,11):
+    print(f'{i} x {j} {i*j}')
